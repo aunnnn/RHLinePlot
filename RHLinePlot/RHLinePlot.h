0b4f1fd648df378cd504cfd78be3e23166029a2e
Wirawit Rueopas <aun.wirawit@gmail.com>
2020-04-09 01:02:37 -0700

    Initial Commit

---
+++ RHLinePlot/RHLinePlot.h
@@ -0,0 +1,19 @@
+//
+//  RHLinePlot.h
+//  RHLinePlot
+//
+//  Created by Wirawit Rueopas on 4/9/20.
+//  Copyright © 2020 Wirawit Rueopas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RHLinePlot.
+FOUNDATION_EXPORT double RHLinePlotVersionNumber;
+
+//! Project version string for RHLinePlot.
+FOUNDATION_EXPORT const unsigned char RHLinePlotVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RHLinePlot/PublicHeader.h>
+
+
